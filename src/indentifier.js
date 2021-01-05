ec1791f99c4e7ca54487df328c6f6f65fd52dba5
exos <exos@kaktheplanet.xyz>
2021-01-05 15:49:06 -0300

    Make each connection indep

---
+++ src/indentifier.js
@@ -0,0 +1,93 @@
+
+import {EventEmitter} from 'events';
+import NickServ from 'nickserv';
+
+class Indentifier extends EventEmitter {
+
+    constructor(plugInstance, connection, connectionOptions) {
+        super();
+        this._plugin = plugInstance;
+        this._connection = connection;
+        this._identified = false;
+        this._nick = connection.nick;
+        this._config = (connectionOptions);
+        this._ts = null;
+
+        this._ns = new NickServ(this._nick, {
+        
+        });
+
+        this.debug(`Attaching identifier to ${this._connection.identifier}`);
+        debugger;
+        this._ns.attach('irc', this._connection.client);
+
+        this._ns.on('loggedout', () => {
+            this.debug(`Logged out, re-identify`);
+            this._identified = false;
+            this.identify();
+        });
+
+        this._ns.on('notice', (notice) => {
+            this.debug('Received notice ' + notice);
+        });
+
+        this.identify();
+    }
+
+    identify() {
+        if (this._identified) {
+            return;
+        }
+
+        this.debug('Identifing');
+
+        this._ts = setTimeout(() => {
+            this.debug('Login timeout');
+            throw new Error('Login timeout');
+        }, 30000);
+
+        this._ns.once('identified', () => {
+            clearTimeout(this._ts);
+            this.debug('Identify success');
+            this._identified = true;
+            this.emit('success');
+        });
+
+        this._ns.identify(this._config.password, (err) => {
+            if (err) {
+                this.debug('Error', err);
+                throw err;
+            }
+        });
+    }
+
+    waitForSuccess() {
+        if (this._identified) {
+            return this.Promise.resolve(false);
+        }
+
+        return new this.Promise((resolve) => {
+            this.debug('Waiting for success');
+            this.once('success', () => {
+                resolve(true);
+            });
+        });
+    }
+
+    get debug() {
+        return this._plugin.debug;
+    }
+
+    get Promise() {
+        return this._plugin.Promise;
+    }
+
+    get isIdentified() {
+        return this._identified;
+    }
+
+    get identifier() {
+        this._connection.identifier;
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,93 +1,6 @@
 
-import {EventEmitter} from 'events';
-import NickServ from 'nickserv';
 import PluginBase from 'ircgrampp-plugin';
-
-class Indentifier extends EventEmitter {
-
-    constructor(plugInstance, connection, connectionOptions) {
-        super();
-        this._plugin = plugInstance;
-        this._connection = connection;
-        this._identified = false;
-        this._nick = connection.nick;
-        this._config = (connectionOptions);
-        this._ts = null;
-
-        this._ns = new NickServ(this._nick, {
-        
-        });
-
-        this.debug(`Attaching identifier to ${this._connection.identifier}`);
-        debugger;
-        this._ns.attach('irc', this._connection.client);
-
-        this._ns.on('loggedout', () => {
-            this.debug(`Logged out, re-identify`);
-            this._identified = false;
-            this.identify();
-        });
-
-        this._ns.on('notice', (notice) => {
-            this.debug('Received notice ' + notice);
-        });
-
-        this.identify();
-    }
-
-    identify() {
-        if (this._identified) {
-            return;
-        }
-
-        this.debug('Identifing');
-
-        this._ts = setTimeout(() => {
-            this.debug('Login timeout');
-            throw new Error('Login timeout');
-        }, 30000);
-
-        this._ns.once('identified', () => {
-            clearTimeout(this._ts);
-            this.debug('Identify success');
-            this._identified = true;
-            this.emit('success');
-        });
-
-        this._ns.identify(this._config.password, (err) => {
-            if (err) {
-                this.debug('Error', err);
-                throw err;
-            }
-        });
-    }
-
-    waitForSuccess() {
-        if (this._identified) {
-            return this.Promise.resolve(false);
-        }
-
-        return new this.Promise((resolve) => {
-            this.debug('Waiting for success');
-            this.once('success', () => {
-                resolve(true);
-            });
-        });
-    }
-
-    get debug() {
-        return this._plugin.debug;
-    }
-
-    get Promise() {
-        return this._plugin.Promise;
-    }
-
-    get isIdentified() {
-        return this._identified;
-    }
-
-}
+import Indentifier from './identifier';
 
 export default class NickServPlugin extends PluginBase {
     
@@ -95,10 +8,11 @@ export default class NickServPlugin extends PluginBase {
         this.debug('NickServ plugin started');
         this._connectionOptions = this.config.get('connections');
         this._autoconnect = false;
+        this._connections = [];
     }
 
     getCompatibleVersion() {
-        return "~0.3.0";
+        return "~0.4.2";
     }
 
     afterIrcconnectionCreate(connection) {
@@ -112,10 +26,11 @@ export default class NickServPlugin extends PluginBase {
 
         if (connectionOptions) {
             this.debug(`Options find to ${options.nick}@${options.server}`);
-            connection.__identifier = new Indentifier(
+            this._connections.push(new Indentifier(
                 this,
                 connection,
-                connectionOptions);
+                connectionOptions
+            ));
         } else {
             this.debug('Not found, ignoring');
         }
@@ -124,8 +39,16 @@ export default class NickServPlugin extends PluginBase {
     }
 
     beforeIrcconnectionWaitForRegistered(connection) {
-        if (connection.__identifier) { 
+        const con = this._connections
+            .find((x) => x.identifier === connection.__identifier)
+
+        if (con) {
+            this.debug(`Waiting for registered of ${con.identifier}`);
             return connection.__identifier.waitForSuccess();
         }
+
+        this.debug(
+            `Waiting for registered of ${connection.__identifier} ignored`
+        );
     }
 }
